6e3b41c48e314afd920dec38e5d2ae640ad7a5c8
Diego Buitrago <die.ar.90@gmail.com>
2021-01-15 09:45:24 -0500

    conexion base de datos

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const actores = require('./routes/actores.js');
+require('dotenv').config();
 
 app.use(morgan('dev'));
+
+//Routes
+app.use('/api', actores);
 
 app.get('/', (req, res) => {
     res.send("<h1>API Academia</h1>");
@@ -14,6 +19,8 @@ app.get('/estudiante', (req, res) => {
     res.json(estudiantes);
 })
 
-app.listen(8083, () => {
-    console.log("Aplicacion corriendo en el puerto 8083!!")
+app.set('port',process.env.PORT || 5000)
+
+app.listen(app.get('port'), () => {
+    console.log(`Aplicacion corriendo en el puerto ${app.get('port')}!!`)
 });
